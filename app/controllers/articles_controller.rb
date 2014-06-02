b359637be6ec2c3ae7c098f908f0ba02cddbcfd6
Caroline Artz <ceartz@gmail.com>
2014-06-02 00:18:40 -0500

    added create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
@@ -6,4 +7,25 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    #fail => see note
+    #MYNOTE: @article = Article.new(params[:article]) => will make blow up for security reasons not a good idea to blindly save parameters sent into us via the params hash (USE STRONG PARAMETERS)
+    # MYNOTE: @article.title = params[:article][:title] #one way
+    @article = Article.new(article_params)  # MYNOTE: fix by putting methods require and permit, usually in helper file
+    @article.save
+    redirect_to article_path(@article)
+  end
+
 end
+
+# MYNOTE: trick to get params: put 'fail' in create and then check the error message:
+# {"utf8"=>"✓",
+#"authenticity_token"=>"HGhK9feovYRoPqUx/1IWLWoCdHq14TTQPhPgTPEQvcI=",
+#  "article"=>{"title"=>"",
+#  "body"=>""},
+#  "commit"=>"Create Article"}
