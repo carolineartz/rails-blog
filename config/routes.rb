31b0d2f8a00f768709ef986c9f239e879b9d185a
Caroline Artz <ceartz@gmail.com>
2014-06-02 09:30:20 -0500

    form-based workflow feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 # when you make a request like http://localhost:3000/articles/, the router will understand you’re looking for a listing of the articles, and http://localhost:3000/articles/new means you’re trying to create a new article.
 
 Rails.application.routes.draw do
+  root to: 'articles#index'
   resources :articles
 end
