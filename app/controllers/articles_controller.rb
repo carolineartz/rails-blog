973706d99a03d6d51190683060fb6cff415d665a
Caroline Artz <ceartz@gmail.com>
2014-06-02 01:49:55 -0500

    added update and delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   include ArticlesHelper
+
   def index
     @articles = Article.all
   end
@@ -18,11 +19,33 @@ class ArticlesController < ApplicationController
     # MYNOTE: @article.title = params[:article][:title] #one way
     @article = Article.new(article_params)  # MYNOTE: fix by putting methods require and permit, usually in helper file
     @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to action: "index"
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
     redirect_to article_path(@article)
   end
 
 end
 
+
+
+#######################NOTES####################
 # MYNOTE: trick to get params: put 'fail' in create and then check the error message:
 # {"utf8"=>"✓",
 #"authenticity_token"=>"HGhK9feovYRoPqUx/1IWLWoCdHq14TTQPhPgTPEQvcI=",
